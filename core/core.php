45ac262d7f8030dd6d4e3962b83bb659cfe4d9a5
aprieto-PC\aprieto <alexander.prieto.chavez@gmail.com>
2018-05-20 22:28:37 -0500

    efg

---
+++ core/core.php
@@ -0,0 +1,10 @@
+<?php
+/*
+  EL NUCLUEO DE LA APLICACIÓN
+*/
+define('HTML_DIR','html/');
+define('APP_TITLE','OcrendBB Software');
+define('APP_COPY','Copyright &copy; ' . date('Y',time()) . ' Ocrend Software. ')
+
+require('vendor/autoload.php');
+?>
